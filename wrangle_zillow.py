4a6b29f5e1976c04d8f909967a5161e6c41d3879
Amanda Gomez <o0amandagomez0o@gmail.com>
2021-04-04 17:21:21 -0500

    update

--- wrangle_zillow.py
+++ wrangle_zillow.py
@@ -69,6 +69,9 @@ def zillow17():
     and longitude IS NOT NULL
     and transactiondate like '2017%'
     """
+    
+    
+    
     return pd.read_sql(query, get_connection('zillow'))
 
 
@@ -549,3 +552,30 @@ def wrangle_mall():
     
     #SCALE   
     return min_max_scaler(train, validate, test)
+
+SELECT prop.*,
+       pred.logerror,
+       pred.transactiondate,
+       air.airconditioningdesc,
+       arch.architecturalstyledesc,
+       build.buildingclassdesc,
+       heat.heatingorsystemdesc,
+       landuse.propertylandusedesc,
+       story.storydesc,
+       construct.typeconstructiondesc
+FROM   properties_2017 prop
+INNER JOIN (SELECT parcelid,
+		           logerror,
+                   Max(transactiondate) transactiondate
+            FROM   predictions_2017
+            GROUP  BY parcelid, logerror) pred
+         USING (parcelid)
+LEFT JOIN airconditioningtype air USING (airconditioningtypeid)
+LEFT JOIN architecturalstyletype arch USING (architecturalstyletypeid)
+LEFT JOIN buildingclasstype build USING (buildingclasstypeid)
+LEFT JOIN heatingorsystemtype heat USING (heatingorsystemtypeid)
+LEFT JOIN propertylandusetype landuse USING (propertylandusetypeid)
+LEFT JOIN storytype story USING (storytypeid)
+LEFT JOIN typeconstructiontype construct USING (typeconstructiontypeid)
+WHERE prop.latitude IS NOT NULL
+      AND prop.longitude IS NOT NULL;
